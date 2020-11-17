e37ab6be6f94d71f68d5db3183b1b824ce0ba4f0
Martin Lynge Rasmussen <44898337+martinlyngerasmussen@users.noreply.github.com>
2020-11-17 12:52:24 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,21 +2,21 @@ from baidu_index.utils import test_cookies
 from baidu_index import config
 from baidu_index import BaiduIndex, ExtendedBaiduIndex
 
-cookies = """这里放cookie"""
+cookies = """put cookie here"""
 
 if __name__ == "__main__":
-    # 测试cookies是否配置正确
-    # True为配置成功，False为配置不成功
+    # Test whether the cookies are configured correctly
+    # True means the configuration is successful, False means the configuration is unsuccessful
     print(test_cookies(cookies))
 
     keywords = [['英雄联盟'], ['冠军杯', '英雄联盟'], ['抑郁', '自杀', '明星']]
 
-    # 获取城市代码, 将代码传入area可以获取不同城市的指数, 不传则为全国
-    # 媒体指数不能分地区获取
+   # Get the city code, pass the code into the area to get the index of different cities, if not pass it for the whole country
+   # Media index cannot be obtained by region
     print(config.PROVINCE_CODE)
     print(config.CITY_CODE)
 
-    # 获取百度搜索指数(地区为山东)
+    # Get Baidu search index (area=901 is Shandong province)
     # index: {'keyword': ['抑郁', '自杀', '明星'], 'type': 'wise', 'date': '2018-06-10', 'index': '1835'}
     baidu_index = BaiduIndex(
         keywords=keywords,
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     for index in baidu_index.get_index():
         print(index)
     
-    # 获取百度媒体指数
+    # Get Baidu Media Index
     # index: {'keyword': ['抑郁', '自杀', '明星'], 'date': '2018-12-29', 'index': '0'}
     news_index = ExtendedBaiduIndex(
         keywords=keywords,
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     for index in news_index.get_index():
         print(index)
 
-    # 获取百度咨询指数
+    # Get Baidu consulting index
     # index: {'keyword': ['抑郁', '自杀', '明星'], 'date': '2018-12-29', 'index': '1102911'}
     feed_index = ExtendedBaiduIndex(
         keywords=keywords,
